1ec41f1ccce6eaf80e426dcfd6112afd6eba1c71
Bartosz Bryniarski <brylka@jelenia.com>
2024-11-24 13:05:55 +0100

    niedziela

--- niedziela_2024_11_24/5_csv.py
+++ niedziela_2024_11_24/5_csv.py
@@ -1,25 +1,43 @@
 import csv
 
-def pobierz_dane_z_csv(nazwa_pliku = "dane.csv", separator=','):
-    lista = []
-    with open(nazwa_pliku, mode='r', encoding='utf-8') as plik:
-        linie = csv.DictReader(plik, delimiter=separator)
-        for wiersz in linie:
-            lista.append(wiersz)
-    return lista
+class Dane:
+    def __init__(self):
+        self.lista = []
+
+    def pobierz_dane_z_csv(self, nazwa_pliku, separator=','):
+        self.lista = []
+        with open(nazwa_pliku, mode='r', encoding='utf-8') as plik:
+            linie = csv.DictReader(plik, delimiter=separator)
+            for wiersz in linie:
+                self.lista.append(wiersz)
+        if len(self.lista) > 0:
+            print(f"Dane wczytane, ilość rekordów: {len(self.lista)}")
+        else:
+            print(f"Coś poszło nie tak.")
+
+    def suma_lat(self):
+        suma = 0
+        for pacjent in self.lista:
+            suma += int(pacjent['wiek'])
+        return suma
+
+dane = Dane()
+dane.pobierz_dane_z_csv("dane.csv")
+print(dane.suma_lat())
 
 
-lista = pobierz_dane_z_csv("dane.csv")
-print(lista)
-suma = 0
-max = len(lista[0]["nazwisko"])
-nazwisko = lista[0]["nazwisko"]
-for pacjent in lista:
-    suma += int(pacjent['wiek'])
-    if max < len(pacjent["nazwisko"]):
-        max = len(pacjent["nazwisko"])
-        nazwisko = pacjent["nazwisko"]
 
-print(f"Suma lat wynosi {suma}")
-print(f"Średni wiek wynosi {suma/len(lista):.0f}")
-print(f"Najdłuższe nazwisko: {nazwisko}")
+
+
+
+
+# max = len(lista[0]["nazwisko"])
+# nazwisko = lista[0]["nazwisko"]
+# for pacjent in lista:
+#     if max < len(pacjent["nazwisko"]):
+#         max = len(pacjent["nazwisko"])
+#         nazwisko = pacjent["nazwisko"]
+#
+# print(f"Suma lat wynosi {suma}")
+# print(f"Średni wiek wynosi {suma/len(lista):.0f}")
+# print(f"Najdłuższe nazwisko: {nazwisko}")
