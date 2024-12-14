749547f3c8b744f273beca8f0149347ac6f7085c
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-14 11:09:26 +0100

    sobota 14.12.2024

---
+++ sobota_2024_12_14/6_game_of_life.py
@@ -0,0 +1,64 @@
+from time import sleep
+
+class GameOfLife:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.grid = []
+
+        for y in range(self.height):
+            row = []
+            for x in range(self.width):
+                row.append(0)
+            self.grid.append(row)
+
+    def set_cell(self, x, y):
+        self.grid[y-1][x-1] = not self.grid[y-1][x-1]
+
+    def count_neighbords(self, x, y):
+        count = 0
+        for j in range(max(0,y-1),min(self.height,y+2)):
+            for i in range(max(0,x-1),min(self.width,x+2)):
+                if self.grid[j][i] and (x != i or y != j):
+                    count += 1
+        return count
+
+    def update(self):
+        new_grid = []
+        for y in range(self.height):
+            row = []
+            for x in range(self.width):
+                row.append(0)
+            new_grid.append(row)
+
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.grid[y][x] == 1:
+                    if self.count_neighbords(x, y) == 2 or self.count_neighbords(x, y) == 3:
+                        new_grid[y][x] = 1
+                else:
+                    if self.count_neighbords(x, y) == 3:
+                        new_grid[y][x] = 1
+        self.grid = new_grid
+
+    def display(self):
+        for y in self.grid:
+            for x in y:
+                if x == 0:
+                    print('`', end=" ")
+                else:
+                    print('*', end=" ")
+            print()
+
+
+
+
+game = GameOfLife(10,10)
+game.set_cell(4,4)
+game.set_cell(5,4)
+game.set_cell(6,4)
+
+while True:
+    game.display()
+    game.update()
+    sleep(1)
