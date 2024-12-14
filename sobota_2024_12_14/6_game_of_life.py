406a51c759a526ba3ef6cb15f43c8867d0ae65b0
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-14 12:15:04 +0100

    sobota 14.12.2024

--- sobota_2024_12_14/6_game_of_life.py
+++ sobota_2024_12_14/6_game_of_life.py
@@ -5,33 +5,43 @@ class GameOfLife:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.grid = []
-
-        for y in range(self.height):
-            row = []
-            for x in range(self.width):
-                rnd = random.randint(0,1)
-                row.append(rnd)
-            self.grid.append(row)
+        self.grid = [[0 for x in range(self.width)] for y in range(self.height)]
+        # self.grid = []
+        #
+        # for y in range(self.height):
+        #     row = []
+        #     for x in range(self.width):
+        #         row.append(0)
+        #     self.grid.append(row)
 
     def set_cell(self, x, y):
         self.grid[y-1][x-1] = not self.grid[y-1][x-1]
 
     def count_neighbords(self, x, y):
         count = 0
-        for j in range(max(0,y-1),min(self.height,y+2)):
-            for i in range(max(0,x-1),min(self.width,x+2)):
+        for j in range(y - 1, y + 2):
+            if j == self.height:
+                j = 0
+            for i in range(x - 1, x + 2):
+                if i == self.width:
+                    i = 0
                 if self.grid[j][i] and (x != i or y != j):
                     count += 1
+
+        # for j in range(max(0,y-1),min(self.height,y+2)):
+        #     for i in range(max(0,x-1),min(self.width,x+2)):
+        #         if self.grid[j][i] and (x != i or y != j):
+        #             count += 1
         return count
 
     def update(self):
-        new_grid = []
-        for y in range(self.height):
-            row = []
-            for x in range(self.width):
-                row.append(0)
-            new_grid.append(row)
+        new_grid = [[0 for x in range(self.width)] for y in range(self.height)]
+        # new_grid = []
+        # for y in range(self.height):
+        #     row = []
+        #     for x in range(self.width):
+        #         row.append(0)
+        #     new_grid.append(row)
 
         for y in range(self.height):
             for x in range(self.width):
@@ -55,10 +65,10 @@ class GameOfLife:
 
 
 
-game = GameOfLife(30,30)
-# game.set_cell(22,22)
-# game.set_cell(23,22)
-# game.set_cell(24,22)
+game = GameOfLife(10,10)
+game.set_cell(3,0)
+game.set_cell(4,0)
+game.set_cell(5,0)
 # game.set_cell(22,23)
 # game.set_cell(23,24)
 
