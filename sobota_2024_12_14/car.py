417fa2f1dfef089c935c7604e75513446170de46
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-14 13:03:50 +0100

    sobota 14.12.2024

--- sobota_2024_12_14/car.py
+++ sobota_2024_12_14/car.py
@@ -1,13 +1,14 @@
 from vehicle import Vehicle
 
 class Electric(Vehicle):
-    def __init__(self, mark, model, year, distance, battery):
-        super().__init__(mark, model, year)
+    def __init__(self, mark, model, year, distance, battery, color):
+        super().__init__(mark, model, year, color)
         self.distance = distance
         self.battery = battery
 
 
 if __name__ == "__main__":
-    electric = Electric('Tesla', '3', 2024, 200, 50)
+    electric = Electric('Tesla', '3', 2024, 200, 50, "niebieski")
     print(electric.mark)
     electric.hong()
+    print(electric.color)
--- sobota_2024_12_14/vehicle.py
+++ sobota_2024_12_14/vehicle.py
@@ -1,15 +1,19 @@
 class Vehicle:
-    def __init__(self, mark, model, year):
+    def __init__(self, mark, model, year, color=None):
         self.mark = mark
         self.model = model
         self.year = year
+        self.color = color
     def __str__(self):
-        return f"{self.mark} {self.model} {self.year}"
+        return f"{self.mark} {self.model} {self.year} {self.color}"
 
     def hong(self):
         print(f"Samochód {self.mark} trąbi")
 
+    def change_color(self,new_color):
+        self.color = new_color
+
 
 if __name__ == "__main__":
-    vehicle = Vehicle("Fiat", "126p", 1970)
+    vehicle = Vehicle("Fiat", "126p", 1970, "zielony")
     print(vehicle)
