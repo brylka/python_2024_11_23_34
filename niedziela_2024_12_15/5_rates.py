22042396076942e1d20204bb35e091a10414228c
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-15 13:31:57 +0100

    niedziela 15.12.2024

--- niedziela_2024_12_15/5_rates.py
+++ niedziela_2024_12_15/5_rates.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Witaj świecie!"
+    return render_template("index.html")
 
 @app.route('/rates')
 def get_rates():
@@ -13,15 +13,15 @@ def get_rates():
 
     response = requests.get(url)
     data = response.json()
-    base = data['base']
-    date = data['date']
+    # base = data['base']
+    # date = data['date']
     rates = data['rates']
-    pln = 100
+    # pln = 100
 
-    text = ''
-    for a, b in rates.items():
-        text += f"{pln}PLN to: {b:10.4f} \t\t{pln * float(b):10.2f}{a}"
-    return text
+    # text = ''
+    # for a, b in rates.items():
+    #     text += f"{pln}PLN to: {b:10.4f} \t\t{pln * float(b):10.2f}{a}<br>"
+    return render_template("rates.html", a=rates)
 
 
 if __name__ == '__main__':
