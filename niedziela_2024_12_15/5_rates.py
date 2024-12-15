713c4d9e24e64aed04818d4507eca71350848261
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-15 14:13:13 +0100

    niedziela 15.12.2024

--- niedziela_2024_12_15/5_rates.py
+++ niedziela_2024_12_15/5_rates.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -10,12 +10,29 @@ def hello():
 @app.route('/rates')
 def get_rates():
     url = "https://api.frankfurter.dev/v1/latest?base=PLN"
-
     response = requests.get(url)
     data = response.json()
     rates = data['rates']
 
     return render_template("rates.html", a=rates)
+
+@app.route('/convert', methods=['GET', 'POST'])
+def convert():
+    url = "https://api.frankfurter.dev/v1/latest?base=PLN"
+    response = requests.get(url)
+    data = response.json()
+    rates = data['rates']
+
+    if request.method == 'POST':
+        try:
+            pln = float(request.form['pln'])
+        except:
+            pln = 0
+        currency = request.form['currency']
+        result = rates[currency]*pln
+        return str(result)
+    else:
+        return render_template("convert.html", a=rates)
 
 
 if __name__ == '__main__':
