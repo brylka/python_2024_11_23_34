95e0b6a6c9024e59e3cf5508ba05a419fc1aa063
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-15 12:38:10 +0100

    niedziela 15.12.2024

--- niedziela_2024_12_15/4_rates.py
+++ niedziela_2024_12_15/4_rates.py
@@ -1,6 +1,5 @@
 import requests
 
-
 url = "https://api.frankfurter.dev/v1/latest?base=PLN"
 
 response = requests.get(url)
@@ -11,4 +10,7 @@ rates = data['rates']
 
 #print(base, date, rates['EUR'])
 pln = float(input("Podaj kowotę w PLN: "))
-print(f"Kwota {pln}PLN to {pln*float(rates['EUR'])}EURO")
+#print(f"Kwota {pln}PLN to {pln*float(rates['EUR'])}EURO")
+
+for a, b in rates.items():
+    print(f"{pln}PLN to: {b:10.4f} \t\t{pln*float(b):10.2f}{a}")
